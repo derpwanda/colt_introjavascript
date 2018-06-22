6ef78cb3820d9fdbd47b593b34b16123c5be19e9
indigoblow <indigoblow@gmail.com>
2018-06-22 10:27:23 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var firstUserName = prompt("Hi, what is your first name?");
+var lastUserName = prompt("Can I have your last name?");
+var ageOfUser = prompt("And finally...your age?");
+console.log("Your name is " + firstUserName + " " + lastUserName + ".");
+console.log("You are " + ageOfUser + " years old.");
